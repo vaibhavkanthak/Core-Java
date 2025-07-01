027ad585a6d5a97d9a9123e9222c725dbfa53ff7
vaibhav_kanthak <vaibhavkanthak@gmail.com>
2025-07-01 21:29:11 +0530

    First Commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+    }
+
+}
