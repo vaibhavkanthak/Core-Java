ec2ab58ac63760a6c75ad4616204483a187fd516
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-07-13 22:36:01 +0530

    Write a Java program to print a face.

---
+++ src/basic/FacePrinter.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class FacePrinter {
+
+    public static void main(String[] args){
+
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
