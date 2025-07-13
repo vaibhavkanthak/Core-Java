d2f1e6abe11e57eaa91466b7d4f9d34d280bef36
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-07-13 22:36:49 +0530

    Write a Java program to print an American flag on the screen.

---
+++ src/basic/FlagDisplay.java
@@ -0,0 +1,25 @@
+package basic;
+
+public class FlagDisplay {
+
+    public static void main(String[] args){
+
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+
+        // Print a row of equal signs to complete the design
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
