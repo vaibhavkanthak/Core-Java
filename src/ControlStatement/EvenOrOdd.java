4220e9d716f9aba0d994ff3142212e67f28f0f7b
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-07-21 21:11:56 +0530

    Write a Java program which determines the given number even or odd.

---
+++ src/ControlStatement/EvenOrOdd.java
@@ -0,0 +1,20 @@
+package ControlStatement;
+import java.util.Scanner;
+public class EvenOrOdd
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n % 2 == 0)
+        {
+            System.out.println("The given number "+n+" is Even ");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is Odd ");
+        }
+    }
+}
