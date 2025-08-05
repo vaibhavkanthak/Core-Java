114fba1a92afed086a841e796eb8c9073773aea9
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-08-05 20:58:37 +0530

    Write a Java program swap numbers without third variable.

--- src/basic/SwapTwoNumbers02.java
+++ src/basic/SwapTwoNumbers02.java
@@ -6,13 +6,13 @@ public class SwapTwoNumbers02 {
 
         int a, b;
         a = 15;
-        b = 27;
+        b = 29;
         System.out.println("Before swapping:- a, b = " + a + ", " + b);
         a = a + b;  // Add a and b and store the result in a
         b = a - b;  // Subtract the original b from the new a and store the result in b
         a = a - b;  // Subtract the new b from the updated a and store the result in a
 
-        System.out.println("After swapping: a, b = " + a + ", " + b);
+        System.out.println("After swapping:- a, b = " + a + ", " + b);
 
     }
 }
