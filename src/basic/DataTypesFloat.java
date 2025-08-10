fd49410e8ab88f81ef6ed28a9f032d4f72db7f67
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-08-10 21:33:43 +0530

    Basic-java Data types programs

---
+++ src/basic/DataTypesFloat.java
@@ -0,0 +1,12 @@
+//Java program to illustrate float primitive data type
+class DataTypesFloat{
+		
+		public static void main(String[] args){
+		
+		float value = 3.14f;
+		
+		System.out.println(value);
+	}
+}
+
+		 
