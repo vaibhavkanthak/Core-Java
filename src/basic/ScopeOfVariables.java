989a466e1b7c48a06aac9ab28d712b980dc719a2
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-08-13 21:18:10 +0530

    Java program to show scope of instance and static variables

---
+++ src/basic/ScopeOfVariables.java
@@ -0,0 +1,22 @@
+//Java program to show scope of instance and static variables
+package basic;
+public class ScopeOfVariables {
+    static int x = 11;
+    private int y = 33;
+    public void method1(int x)
+    {
+        ScopeOfVariables t = new ScopeOfVariables();
+        this.x = 22;
+        y = 44;
+
+        System.out.println("ScopeOfVariables.x: " + this.x);
+        System.out.println("t.x: " + t.x);
+        System.out.println("t.y: " + t.y);
+        System.out.println("y: " + y);
+    }
+
+    public static void main(String args[]) {
+        ScopeOfVariables t = new ScopeOfVariables();
+        t.method1(5);
+    }
+}
