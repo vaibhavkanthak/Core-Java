c513b02a728e805982e59ae8819d7c09afd2e068
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-08-16 17:24:25 +0530

    Creating Wrapper Objects using Constructors (deprecated way)

---
+++ src/basic/WrapperDemo2.java
@@ -0,0 +1,14 @@
+//Creating Wrapper Objects using Constructors (deprecated way)
+package basic;
+
+public class WrapperDemo2 {
+    public static void main(String[] args){
+
+        Integer i = new Integer(100);
+        Double d = new Double("12.34");
+
+        System.out.println("Integer: " +i);
+        System.out.println("Double: " + d);
+
+    }
+}
