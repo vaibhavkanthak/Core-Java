f9583a7ef264deef1332862a7f2c44a85dc9a47d
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-08-30 20:11:59 +0530

    Practice

--- src/PracticeProblem.java
+++ src/PracticeProblem.java
@@ -1,12 +1,37 @@
+import static java.lang.System.out;
 public class PracticeProblem {
 
 
     public static void main(String[] args){
 
-        System.out.println("\nThis is after newline");
+        out.println("\nThis is after newline");
         String emoji = "\uD83D\uDE03";
-        System.out.println(emoji);
+        out.println(emoji);
+        out.println("java\u2122");
 
+        String s = "java";
+        String s1 = s.substring(0, 1);
+        out.println(s1);
+        out.println(s1 + " ");
+        //1
+        char[] arr = {'j', 'a', 'v', 'a'};
+        out.println(arr);
+        //2
+        String a = "geeky";
+        out.println(a);
+        out.println(a.length());
+        out.println(a.charAt(3));
+        out.println(a.substring(3));
+        out.println(a.substring(0, 4));
+        //3
+        String b = new String("Nomaad");
+        out.println(b);
+        //4
+        StringBuffer sf = new StringBuffer("Geekyway");
+        out.println(sf);
+        //5
+        StringBuilder sb = new StringBuilder("Nomaadicway");
+        out.println(sb);
 
     }
 
