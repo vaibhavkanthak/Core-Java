092a610a03c8c1c818b1085a19c69d9f688652e6
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-07 11:17:16 +0530

    Java string methods demo examples.

--- src/basic/JavaStringMethods.java
+++ src/basic/JavaStringMethods.java
@@ -17,5 +17,13 @@ public class JavaStringMethods {
             // move 2 code points ahead starting from index 8
             int cpCount = text.offsetByCodePoints(8, 2);
             System.out.println(cpCount);
+
+            //method 4
+            String str1 = "Apple";
+            String str2 = "Orange";
+            String str3 = "Apple";
+            System.out.println("str1.compareTo(str2): " + str1.compareTo(str2));
+            System.out.println("str2.compareTo(str1): " + str2.compareTo(str1));
+            System.out.println("str1.compareTo(str3):" + str1.compareTo(str3));
         }
 }
