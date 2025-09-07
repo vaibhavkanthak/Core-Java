3e0b92dc14228fa2d64df3c4ea794f6658e89cb7
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-07 13:53:55 +0530

    Java string methods demo examples.

---
+++ src/basic/JavaStringsMethods2.java
@@ -0,0 +1,30 @@
+package basic;
+
+public class JavaStringsMethods2 {
+    public static void main(String[] args) {
+        String text = "This is a simple string example.";
+
+        // 1. indexOf(String str)
+        int index1 = text.indexOf("simple");
+        System.out.println("indexOf(\"simple\"): " + index1);
+
+        // 2. indexOf(String str, int fromIndex)
+        int index2 = text.indexOf("is", 3);
+        System.out.println("indexOf(\"is\", 3): " + index2);
+
+        // 3. indexOf(int cp)
+        int index3 = text.indexOf('s'); // using char (treated as code point)
+        System.out.println("indexOf('s'): " + index3);
+
+        // 4. indexOf(int cp, int fromIndex)
+        int index4 = text.indexOf('s', 5);
+        System.out.println("indexOf('s', 5): " + index4);
+
+        // 5. When substring/codepoint not found
+        int index5 = text.indexOf("notfound");
+        System.out.println("indexOf(\"notfound\"): " + index5);
+
+        int index6 = text.indexOf('z');
+        System.out.println("indexOf('z'): " + index6);
+    }
+}
