40a748a0c26bf11983f8554461fa4d1dca4e146d
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-14 17:47:38 +0530

    if else demo examples

--- src/basic/IfElseDemo.java
+++ src/basic/IfElseDemo.java
@@ -2,6 +2,14 @@ package basic;
 
 public class IfElseDemo {
         public static void main(String[] args){
+            int marks = 45;
+
+            if(marks >= 50){
+                System.out.println("Pass");
+            } else {
+
+                System.out.println("Fail");
+            }
 
         }
 }
