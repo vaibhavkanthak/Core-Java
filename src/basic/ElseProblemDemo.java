f8990ee0ad62c142948a15c207e21d4c10a31964
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-14 17:53:53 +0530

    if else demo examples

---
+++ src/basic/ElseProblemDemo.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class ElseProblemDemo {
+        public static void main(String[] args){
+
+            int x = -5;
+            if(x <= 0){
+                if (x == 0 ){
+                    System.out.println("zero");
+                }else{
+                    System.out.println("Negative");
+                }
+
+            }
+        }
+}
