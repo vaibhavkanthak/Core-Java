b1a541beeb9715e1ada57fc293d082bc640c8f31
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-14 17:43:25 +0530

    if else demo examples

---
+++ src/basic/IfDemo.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class IfDemo {
+        public static void main(String[] args){
+
+            int age = 20;
+            if(age >= 18){
+                System.out.println("You are eligible to vote.");
+            }
+        }
+}
---
+++ src/basic/IfElseDemo.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class IfElseDemo {
+        public static void main(String[] args){
+
+        }
+}
