795768cd023e23df31649e62bc53c39802c24865
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-14 20:22:27 +0530

    java ternary operator demo example.

--- src/basic/TernaryOperatoryDemo1.java
+++ src/basic/TernaryOperatoryDemo1.java
@@ -3,7 +3,7 @@ package basic;
 public class TernaryOperatoryDemo1 {
         public static void main(String[] args){
             int number = -5;
-            int sign = (number >= 0) ? 1 : -1
+            int sign = (number >= 0) ? 1 : -1;
             System.out.println("Sign = " + sign);
         }
 }
