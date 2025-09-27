fe3f1295939b5d02064de063bc353b5bbdccfced
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-09-27 18:21:48 +0530

    java array ragged demo examples

--- src/basic/raggedArrayDemo.java
+++ src/basic/raggedArrayDemo.java
@@ -19,7 +19,7 @@ public class raggedArrayDemo {
                 }
             }
 
-            //Print array now
+            //Print array now.
             for (int i = 0; i < ragged.length; i++) {
                 for (int j = 0; j < ragged[i].length; j++) {
                     System.out.print(ragged[i][j] + " ");
