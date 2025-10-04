029c3d7385c8f9fadcc9c620b655bae23bb4b3a5
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-04 21:09:59 +0530

    java basic practice programs

---
+++ src/practice/practice03.java
@@ -0,0 +1,13 @@
+//Swap two numbers without using a third variable
+package practice;
+
+public class practice03 {
+        public static void main(String[] args){
+            int a = 10, b = 20;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            System.out.println("After Swap: a = " + a + ", b = " + b);
+        }
+}
