ac759cebd814d2dc4889bbc01ddf98bfaf7e690e
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-04 20:53:35 +0530

    java basic practice programs

---
+++ src/practice/practice01.java
@@ -0,0 +1,14 @@
+//Print your name, age, and city
+package practice;
+
+public class practice01 {
+        public static void main(String[] args){
+            String name = "John";
+            int age = 35;
+            String city = "New york";
+
+            System.out.println("Name: " + name);
+            System.out.println("age: " + age);
+            System.out.println("city: " + city);
+        }
+}
---
+++ src/practice/practice02.java
@@ -0,0 +1,8 @@
+//Swap two numbers using a temporary variable
+package practice;
+
+public class practice02 {
+        public static void main(String[] args){
+
+        }
+}
