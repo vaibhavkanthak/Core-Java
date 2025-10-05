1c8ebe99ce5c02ad62aa1f94b749ce385afce74d
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 10:22:36 +0530

    java basic practice programs

---
+++ src/practice/practice04.java
@@ -0,0 +1,10 @@
+//Convert temperature from Celsius to Fahrenheit
+package practice;
+
+public class practice04 {
+        public static void main(String[] args){
+            double celsius = 37.5;
+            double fahrenheit = (celsius * 9/5) + 32;
+            System.out.println(celsius + "°C = " + fahrenheit + "°F");
+        }
+}
