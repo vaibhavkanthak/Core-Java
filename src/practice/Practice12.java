d30756aabd8b9f041d992975881dbd0eeddd731b
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 12:09:07 +0530

    java basic practice programs

---
+++ src/practice/Practice12.java
@@ -0,0 +1,16 @@
+package practice;
+import java.util.Scanner;
+public class Practice12 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int num = sc.nextInt();
+
+            if(num % 2 == 0){
+                System.out.println(num + " is even.");
+            }else {
+                System.out.println(num + " is odd.");
+            }
+            sc.close();
+        }
+}
