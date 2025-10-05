26d776e41d3056e8921df1ce0efda98d821a3a19
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 18:23:26 +0530

    java basic practice programs

---
+++ src/practice/Practice25.java
@@ -0,0 +1,15 @@
+//Find factorial of a number
+package practice;
+import java.util.Scanner;
+public class Practice25 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int num = sc.nextInt();
+            int factorial = 1;
+            for(int i = 1; i <= num; i++){
+                factorial *= i;
+            }
+            System.out.println(factorial);
+        }
+}
