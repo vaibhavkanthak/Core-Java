6d18e2d4ee8e49fb577d0c08fb31682c8d4f5785
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 10:26:31 +0530

    java basic practice programs

---
+++ src/practice/practice05.java
@@ -0,0 +1,11 @@
+//Calculate area and perimeter of a rectangle
+package practice;
+
+public class practice05 {
+        public static void main(String[] args){
+            double length = 10.5, width = 4.2;
+            double perimeter = 2 * (length + width);
+            double area = length * width;
+            System.out.println("Area of rectangle is " + area + " perimeter of rectangle is " + perimeter);
+        }
+}
