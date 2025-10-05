83853c1006b7c7453306b413bd746116ed356549
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 10:38:22 +0530

    java basic practice programs

---
+++ src/practice/practice06.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class practice06 {
+        public static void main(String[] args){
+            double radius = 7.0;
+            double area = Math.PI * radius * radius;
+            double circumference = 2 * Math.PI * radius;
+
+            System.out.println("Area of circle: " + area);
+            System.out.println("circumference of circle: " + circumference);
+
+
+        }
+}
