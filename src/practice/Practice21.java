b590f056542d91b5d7e7e9b319525e30f4044534
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 17:32:44 +0530

    java basic practice programs

---
+++ src/practice/Practice21.java
@@ -0,0 +1,9 @@
+//Print numbers from 1 to 10 using a for loop
+package practice;
+public class Practice21 {
+        public static void main(String[] args){
+            for(int i = 1; i <= 10; i++){
+                System.out.print(i + " ");
+            }
+        }
+}
