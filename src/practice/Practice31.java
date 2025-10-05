2488ec2d13c4a73000b6184207ff78d5e8f9fe9c
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 21:10:24 +0530

    java basic practice programs

---
+++ src/practice/Practice31.java
@@ -0,0 +1,18 @@
+//Count number of digits in a number
+package practice;
+import java.util.Scanner;
+public class Practice31 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int num = sc.nextInt();
+
+            int count = 0;
+            while(num != 0){
+                num /= 10;
+                count++;
+            }
+            System.out.println("Total digits: " + count);
+            sc.close();
+        }
+}
