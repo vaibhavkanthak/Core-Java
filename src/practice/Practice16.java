c4763ab4a77182f4826e63326d31d99c92fefd01
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 13:29:46 +0530

    java basic practice programs

---
+++ src/practice/Practice16.java
@@ -0,0 +1,11 @@
+//Calculate average of three numbers
+package practice;
+public class Practice16 {
+        public static void main(String[] args){
+            double a = 33, b = 55, c = 99;
+            double average = (a + b + c) / 3;
+            System.out.println("Average of three numbers: " + average);
+
+        }
+
+}
