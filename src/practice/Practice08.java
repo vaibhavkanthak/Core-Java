cc16aa4cca21d59490612f68c900fd1461711457
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-05 11:11:14 +0530

    java basic practice programs

---
+++ src/practice/Practice08.java
@@ -0,0 +1,10 @@
+//Find ASCII value of a character
+package practice;
+
+public class Practice08 {
+        public static void main(String[] args){
+            char ch = 'A';
+            int ascii = ch;
+            System.out.println("ascii value of ch: " + ascii);
+        }
+}
