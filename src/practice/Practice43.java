6683e88b5e9377d934a40d94b463f33fb996b035
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-06 18:52:22 +0530

    java basic practice programs

---
+++ src/practice/Practice43.java
@@ -0,0 +1,12 @@
+//Compare two strings
+package practice;
+public class Practice43 {
+        public static void main(String[] args){
+            String s1 = "Java";
+            String s2 = "Java";
+
+            System.out.println(s1.equals(s2));
+            System.out.println(s1.equalsIgnoreCase(s2));
+            System.out.println(s1.compareTo(s2));
+        }
+}
