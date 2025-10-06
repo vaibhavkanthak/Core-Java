0a037458315db06b8abc5a415ac07ce462f6dbc2
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-06 18:56:19 +0530

    java basic practice programs

---
+++ src/practice/Practice44.java
@@ -0,0 +1,9 @@
+//Replace all spaces in a string with hyphens
+package practice;
+public class Practice44 {
+        public static void main(String[] args){
+            String str = "Java is fun coding";
+            String newStr = str.replace(" ", "-");
+            System.out.println(newStr);
+        }
+}
