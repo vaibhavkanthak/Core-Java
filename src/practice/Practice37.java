8aee0c84a17aaf45c0459c928c148d7dbc5bd451
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-06 15:55:05 +0530

    java basic practice programs

---
+++ src/practice/Practice37.java
@@ -0,0 +1,11 @@
+//Input a string and print its length
+package practice;
+
+public class Practice37 {
+        public static void main(String[] args){
+
+            String str = "Java Programming made easy.";
+            System.out.println("Length: " + str.length());
+
+        }
+}
---
+++ src/practice/Practice38.java
@@ -0,0 +1,10 @@
+//Convert a string to uppercase and lowercase
+package practice;
+
+public class Practice38 {
+        public static void main(String[] args){
+            String str = "Java Made easy.";
+            System.out.println("Uppercase: " + str.toUpperCase());
+            System.out.println("Lowercase: " + str.toLowerCase());
+        }
+}
