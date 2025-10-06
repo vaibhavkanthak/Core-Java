08ec5f7272a1afb5bad79bf4c5728d77c9589217
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-06 10:08:19 +0530

    java basic practice programs

---
+++ src/practice/Practice33.java
@@ -0,0 +1,22 @@
+//Print all prime numbers between 1 and 100
+package practice;
+public class Practice33 {
+        public static void main(String[] args){
+           System.out.println("Prime numbers between 1 to 100: ");
+
+           for(int num = 2; num <= 100; num++){
+               boolean isPrime = true;
+               for(int i = 2; i <= num / 2; i++){
+                   if(num % i == 0){
+                       isPrime = false;
+                       break;
+                   }
+
+               }
+               if (isPrime){
+                   System.out.print(num + " ");
+               }
+           }
+
+        }
+}
