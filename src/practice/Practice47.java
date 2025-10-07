4a256eedec494fed168c5587ba576dc794d5e1bd
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-07 21:06:26 +0530

    java basic practice programs

---
+++ src/practice/Practice47.java
@@ -0,0 +1,12 @@
+//Input 5 numbers in an array and print them
+package practice;
+public class Practice47 {
+        public static void main(String[] args){
+            int[] numbers = {10, 20, 30, 40, 50};
+
+            System.out.println("Array elements:");
+            for(int i = 0; i < numbers.length; i++){
+                System.out.println(numbers[i]);
+            }
+        }
+}
