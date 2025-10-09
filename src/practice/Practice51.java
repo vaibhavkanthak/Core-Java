a9d054fd8d25df47d96532b9c9619d028b63880e
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-09 16:50:53 +0530

    java basic practice programs

--- src/practice/Practice51.java
+++ src/practice/Practice51.java
@@ -4,7 +4,7 @@ public class Practice51 {
         public static void main(String[] args){
             int[] arr = {5, 3, 6, 2, 1, 8};
 
-            int temp = 0;
+            int temp;
             for(int i = 0; i < arr.length; i++){
                 for(int j = i + 1; j < arr.length; j++){
                     if(arr[i] > arr[j]){
