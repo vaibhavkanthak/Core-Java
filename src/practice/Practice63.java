e52cd00328e6b80b3a67d92ba4af9dfdb000ec0e
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-10 07:37:49 +0530

    java basic practice programs

---
+++ src/practice/Practice63.java
@@ -0,0 +1,11 @@
+//Demonstrate pre and post increment
+package practice;
+public class Practice63 {
+        public static void main(String[] args){
+            int x = 5;
+            System.out.println("x++ = " + (x++));
+            System.out.println("++x = " + (++x));
+            System.out.println("x = " + x);
+
+        }
+}
