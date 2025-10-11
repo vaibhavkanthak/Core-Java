603a67bb6607394fe1c2c4d5c93a81ad46d01e61
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-11 20:15:14 +0530

    java basic practice programs

---
+++ src/practice/Practice77.java
@@ -0,0 +1,14 @@
+// print all elements of an array
+package practice;
+
+import java.util.Arrays;
+
+public class Practice77 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Array elements are : ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
