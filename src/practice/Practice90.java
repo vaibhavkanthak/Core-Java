8030f2ec3e85403fb27f00e80c47a6dab6009135
vaibhav_kanthak <vaibhav_kanthak@icloud.com>
2025-10-12 18:05:00 +0530

    java basic practice programs

---
+++ src/practice/Practice90.java
@@ -0,0 +1,13 @@
+//Find the missing number in a sequence.
+package practice;
+public class Practice90 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6};
+        int n = arr.length + 1;
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+        for (int num : arr)
+            sum += num;
+        System.out.println("Missing number: " + (total - sum));
+    }
+}
